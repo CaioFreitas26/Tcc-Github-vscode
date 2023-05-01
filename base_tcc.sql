da048bc15ed536e9c2b56a17e35312b8eb394659
CaioFreitas26 <caiohcfreitas@gmail.com>
2023-05-01 20:05:46 -0300

    refactor: alter "site"

---
+++ base_tcc.sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 05-Abr-2023 às 01:55
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `base_tcc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `registros_login`
+--
+
+CREATE TABLE `registros_login` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `data_hora` datetime NOT NULL,
+  `endereco_ip` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `sessoes_login`
+--
+
+CREATE TABLE `sessoes_login` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `data_hora_inicio` datetime NOT NULL,
+  `data_hora_fim` datetime NOT NULL,
+  `endereco_ip` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Acionadores `sessoes_login`
+--
+DELIMITER $$
+CREATE TRIGGER `adicionar_registro_login` AFTER INSERT ON `sessoes_login` FOR EACH ROW BEGIN
+    INSERT INTO registros_login (usuario_id, data_hora, endereco_ip) VALUES (NEW.usuario_id, NEW.data_hora_inicio, NEW.endereco_ip);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tokens_redefinicao_senha`
+--
+
+CREATE TABLE `tokens_redefinicao_senha` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `expiracao` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `registros_login`
+--
+ALTER TABLE `registros_login`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_registros_login_usuario_id` (`usuario_id`);
+
+--
+-- Índices para tabela `sessoes_login`
+--
+ALTER TABLE `sessoes_login`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Índices para tabela `tokens_redefinicao_senha`
+--
+ALTER TABLE `tokens_redefinicao_senha`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `registros_login`
+--
+ALTER TABLE `registros_login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `sessoes_login`
+--
+ALTER TABLE `sessoes_login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tokens_redefinicao_senha`
+--
+ALTER TABLE `tokens_redefinicao_senha`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `registros_login`
+--
+ALTER TABLE `registros_login`
+  ADD CONSTRAINT `registros_login_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Limitadores para a tabela `sessoes_login`
+--
+ALTER TABLE `sessoes_login`
+  ADD CONSTRAINT `sessoes_login_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Limitadores para a tabela `tokens_redefinicao_senha`
+--
+ALTER TABLE `tokens_redefinicao_senha`
+  ADD CONSTRAINT `tokens_redefinicao_senha_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
